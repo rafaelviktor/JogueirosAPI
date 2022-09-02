34bda7b36c0635e5a1c5c9d1f3ed1ada76096544
Rafael Viktor <rafaellviktor@gmail.com>
2022-09-02 14:05:31 -0300

    edição de usuário

--- src/routes/perfil.js
+++ src/routes/perfil.js
@@ -3,6 +3,7 @@ const User = require('../model/Usuario');
 const Anuncio = require('../model/Anuncio');
 const Reserva = require('../model/Reserva');
 const autorizacao = require("../middleware/verificarToken");
+const bcrypt = require('bcrypt');
 
 router.get("/",autorizacao, async (req, res) => {
     try {
@@ -28,6 +29,43 @@ router.get("/minhas-reservas",autorizacao, async (req, res) => {
         res.status(200).json({result: reservas, message: null, success: true});
     } catch (err) {
         res.status(500).json({result: err, message: 'Erro ao pesquisar reservas. Por favor tente novamente.', success: false});
+    }
+})
+
+router.patch("/alterar/:id",autorizacao, async (req, res) => {
+    const id = req.params['id'];
+    try {
+        const usuario = await User.findById(id).exec();
+
+        if(usuario.id === req.id) {
+            const salt = await bcrypt.genSalt(10);
+            const hashedPassword = await bcrypt.hash(req.body.senha, salt);
+            Object.assign(usuario, {nome: req.body.nome, email: req.body.email, senha: hashedPassword, contato: req.body.contato});
+            usuario.save();
+            return res.status(200).json({result: usuario, message: 'Usuário alterado com sucesso.', success: true});
+        }
+
+        res.status(401).json({result: null, message: 'O usuário logado só pode alterar dados da própria conta.', success: false});
+    } catch (err) {
+        res.status(500).json({result: err, message: 'Erro ao alterar o usuário. Por favor, tente novamente.', success: false});
+    }
+})
+
+router.delete("/excluir/:id",autorizacao, async (req, res) => {
+    const id = req.params['id'];
+    try {
+        const usuario = await User.findById(id).exec();
+
+        if(usuario.id === req.id) {
+            await usuario.deleteOne();
+            await Anuncio.deleteMany({id_anunciante: id}).exec();
+            await Reserva.deleteMany({id_usuario: id}).exec();
+            return res.status(200).json({result: null, message: 'Usuário excluído com sucesso.', success: true});
+        }
+
+        res.status(401).json({result: null, message: 'O usuário logado só pode excluir a própria conta.', success: false});
+    } catch (err) {
+        res.status(500).json({result: err, message: 'Usuário não encontrado.', success: false});
     }
 })
 
